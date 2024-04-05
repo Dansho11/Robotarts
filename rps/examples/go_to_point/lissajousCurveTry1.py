d684c2efaf620f05eb6a774b073c718f60dfb943
multivac ac <jhonatan.alvarez@uptc.edu.co>
2024-04-05 15:29:11 -0400

    Firts Push

--- rps/examples/go_to_point/lissajousCurveTry1.py
+++ rps/examples/go_to_point/lissajousCurveTry1.py
@@ -8,18 +8,18 @@ import numpy as np
 import time
 
 # Instantiate Robotarium object
-N = 1
+N = 2
 Pi=np.pi
 initial_conditions = generate_initial_conditions(N)#(x[N];y[N];phi[N])
 
 r = robotarium.Robotarium(number_of_robots=N, show_figure=True, initial_conditions=initial_conditions, sim_in_real_time=False)
 
 t=0
-xpo=t
-ypo=t
+xpo=[0,-0.5]
+ypo=[0, 0]
 t=str(t)
 # Define goal points by removing orientation from poses
-goal_points = np.array(np.mat(t+' ; 0 ; 0'))
+goal_points = np.array(np.mat('0  -1 ; 0  0; 0  0'))
 
 
 # Create single integrator position controller
@@ -41,11 +41,13 @@ r.step()
 
 # While the number of robots at the required poses is less
 # than N...
-while (float(t)<500):
-    xpo=str(xpo)
-    ypo=str(ypo)
-    goal_points = np.array(np.mat(xpo+' ;'+ypo+'; 0'))
-    while (np.size(at_pose(np.vstack((x_si,x[2,:])), goal_points, rotation_error=100)) != N):
+while (float(t)<5000):
+    xpo[0]=str(xpo[0])
+    xpo[1]=str(xpo[1])
+    ypo[0]=str(ypo[0])
+    ypo[1]=str(ypo[1])
+    goal_points = np.array(np.mat(xpo[0]+' '+xpo[1]+' ;'+ypo[0]+' '+ypo[1]+';0 0'))
+    while (np.size(at_pose(np.vstack((x_si,x[2,:])), goal_points, rotation_error=200)) != N):
 
         # Get poses of agents
         x = r.get_poses()
@@ -67,17 +69,17 @@ while (float(t)<500):
 
     #Call at end of script to print debug information and for your script to run on the Robotarium server properly
     t=float(t)+1
-    thao=t/1000
-    a=1
-    b=80
-    c=1
-    d=80
-    xpo=(np.cos(a*thao)-np.power(np.cos(b*thao),3))/2
-    ypo=(np.sin(c*thao)-np.power(np.sin(d*thao),3))/2
-    rpos=r.get_poses()
-    r.step()
+    thao=t/100
+    Pi=np.pi
+    a=[1,3,2,3]
+    xpo[0]=0.2*np.sin(a[0]*thao)
+    ypo[0]=0.2*np.sin(a[1]*thao+Pi/2)
+    xpo[1]=0.2*np.sin(a[2]*thao)-0.6
+    ypo[1]=0.2*np.sin(a[3]*thao+Pi/2)
+    plt.plot(xpo[0],ypo[0],'ro')
     print(t)
-    color='ro'
-    if t>10:
-        plt.plot(rpos[0], rpos[1], color)
+    #xpo=(np.cos(a*thao)-np.power(np.cos(b*thao),3))/2
+    #ypo=(np.sin(c*thao)-np.power(np.sin(d*thao),3))/2
+    #xpo=(16*np.power(np.sin(thao),3))/20
+    #ypo=(13*np.cos(thao)-5*np.cos(2*thao)-2*np.cos(3*thao)-np.cos(4*thao))/20
 r.call_at_scripts_end()
